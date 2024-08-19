55b001abf20c675921ebfd73181a5be4cf426cc9
uchiha_vivek <vs160125@gmail.com>
2024-08-20 02:30:17 +0530

    first-half-smart-contract

---
+++ contracts/Todo.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract Todo {
+    enum TaskStatus {Pending, Finished}
+    address owner;
+    
+    struct Task {
+        string desc;
+        TaskStatus status;
+    }
+    
+    Task[] public tasks;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner { 
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    function addTask(string memory _desc) public onlyOwner {
+        tasks.push(Task(_desc, TaskStatus.Pending));
+    }
+
+    function markAsFinished(uint256 id) public onlyOwner {
+        require(id < tasks.length, "Invalid task ID!");
+        tasks[id].status = TaskStatus.Finished;
+    }
+
+    function getTaskCount() public view returns (uint256) {
+        return tasks.length;
+    }
+
+    function getAllTasks() public view returns (Task[] memory) {
+        return tasks;
+    }
+
+    function getTask(uint256 id) public view returns (string memory, TaskStatus) {
+        require(id < tasks.length, "Invalid task ID!");
+        return (tasks[id].desc, tasks[id].status);
+    }
+}
